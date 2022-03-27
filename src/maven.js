77452c237b40722f546cc13e4e0c568023725862
lib3rtus <lib3rtus@gmail.com>
2022-03-27 22:31:34 +0200

    Added mappers

--- src/maven.js
+++ src/maven.js
@@ -1,20 +1,45 @@
 const path = require('path')
 const exec = require('./exec')
 const { MAVEN_LIST } = require('./paths')
+const mappers = require('../bin/maven/maven-mappers.json')
 
 module.exports.list = async (projectRootPath) => {
   const pomPath = path.join(projectRootPath, 'pom.xml')
-
   const stdOut = await exec(MAVEN_LIST, pomPath)
-
-  return stdOut.split(/\r\n|\n/)
-    .map(line => line.trim().split('--')[0])
-    .filter(dependency => !dependency.endsWith('test'))
-    .map(dependency => {
-      const [groupId, artifactId, pkg, version, scope] = dependency.split(':')
-      return {
-        product: artifactId,
-        version
-      }
-    })
+  const dependencies = extractDependencies(stdOut)
+  return useMapper(dependencies)
 }
+
+const extractDependencies = (stdOut) => stdOut.split(/\r\n|\n/)
+  .map(line => line.split('--')[0].trim())
+  .filter(dependency => !dependency.endsWith('test'))
+  .map(dependency => {
+    const [groupId, artifactId, pkg, version, scope] = dependency.split(':')
+    return {
+      artifactId,
+      groupId,
+      version
+    }
+  })
+
+const useMapper = (dependencies) => {
+  return dependencies.map(mapDependency)
+}
+
+const mapDependency = (dependency) => {
+
+  for (const { groupId, artifactId, cpe } of mappers) {
+    if (groupId === dependency.groupId && (artifactId === '*' || artifactId === dependency.artifactId)) {
+      // console.debug(`Override ${dependency.artifactId} with ${cpe}.`)
+      return {
+        product: cpe,
+        version: dependency.version
+      }
+    }
+  }
+  return {
+    product: dependency.artifactId,
+    version: dependency.version
+  }
+}
+
